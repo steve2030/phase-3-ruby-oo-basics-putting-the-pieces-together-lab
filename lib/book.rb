3cd2d48f68f0983e72c0dac63a06fb262174d33f
steve <stevemighty2030@gmail.com>
2022-10-23 15:17:49 +0300

    the lab is complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
+
+  attr_accessor :author, :page_count, :genre, :turn_page
+  attr_reader :title
+
+  def initialize(title)
+    @title=title
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+book=Book.new("And Then There Were None")
+book.author="Agatha Christie"
+book.page_count=272
+book.genre="Mystery"
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class Shoe
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+  def initialize(brand)
+    @brand=brand
+  end
+  def cobble()
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+
+
+  end
+end
+shoe=Shoe.new("Adidas")
+shoe.color="red"
+shoe.size="9.5"
+shoe.material="suede"
+shoe.condition="tattered"
+
+shoe.cobble
